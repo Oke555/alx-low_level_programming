ce64a02c61053f99005b1288ea1b0de2bddfad9c
Oke555 <ubaghaokenwa@gmail.com>
2022-03-11 10:43:18 -0800

    My first commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -11,21 +11,13 @@
 
 
 int main(void)
-
 {
+	char low;
 
-		char low;
+	for (low = 'a'; low <= 'z'; low++)
 
+		putchar(low);
+	putchar('\n');
 
-
-			for (low = 'a'; low <= 'z'; low++)
-
-						putchar(low);
-
-					putchar('\n');
-
-
-
-						return (0);
-
+	return (0);
 }
